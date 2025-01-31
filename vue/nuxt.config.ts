72e6ffd43f125175065ba20d3d493fc494da6c64
Rungrot <rungrot.muangchalerm@gmail.com>
2025-02-01 05:14:58 +0700

    first commit

--- vue/nuxt.config.ts
+++ vue/nuxt.config.ts
@@ -1,5 +1,19 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/npm/bootstrap/css/bootstrap.min.css'
+        }
+      ],
+      script: [
+        {
+          src: 'https://cdn.jsdelivr.net/npm/bootstrap/js/bootstrap.bundle.min.js',
+          tagPosition: 'bodyClose'
+        }
+      ]
+    }
+  },
+  css: ['@/assets/main.css']
 })
