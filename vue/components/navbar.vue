7d927578c479b8673929bcf5855781f06a739e3b
Rungrot <rungrot.muangchalerm@gmail.com>
2025-02-01 05:26:47 +0700

    first commit

---
+++ vue/components/navbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <!-- Bootstrap Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/">My Nuxt App</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <NuxtLink class="nav-link" to="/">Home</NuxtLink>
+                        </li>
+                        <li class="nav-item">
+                            <NuxtLink class="nav-link" to="/about">About</NuxtLink>
+                        </li>
+                        <li class="nav-item">
+                            <NuxtLink class="nav-link" to="/contact">Contact</NuxtLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
--- vue/pages/index.vue
+++ vue/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
     <div>
-        hello
+        <navbar></navbar>
     </div>
 </template>
