952c3a289a9b1229390be869ed06e160fc49f73e
Rungrot <rungrot.muangchalerm@gmail.com>
2025-02-09 01:37:52 +0700

    Your commit message

--- node/routes/RoutesAPI.js
+++ node/routes/RoutesAPI.js
@@ -1,13 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Example route
-router.get('/', (req, res) => {
-    res.send('API is working');
-});
+router.get("/", require("../API/example"));
+// router.get('/2', (req, res) => {
+//     res.send('API is working');
+// });
 
 // Ensure the required module exports a function
-const registerRoute = require('../API/register');
-router.post('/register', registerRoute);
+const registerRoute = require("../API/register");
+router.post("/register", registerRoute);
+router.get("/register", require("../API/register"));
 
 module.exports = router;
